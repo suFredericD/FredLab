9fb16d094f8580085fece9dc95e0305bc1a34cac
suFredericD <ilric6666@gmail.com>
2019-12-11 09:28:54 +0100

    Release : page WebFonts 	ToDo : 		encart de présentation de la page 		présentation sur index.php
    	modifié :         config/css/coding.css
    	modifié :         config/css/general.css
    	modifié :         scripts/paging/webFonts.php

--- scripts/paging/webFonts.php
+++ scripts/paging/webFonts.php
@@ -8,7 +8,7 @@
  *              Contexte :   php 7.3
  *              Fonction :   construction de la vue : webfonts
  *   Date mise en oeuvre :   10/12/2019
- *          Dernière MàJ :   10/12/2019
+ *          Dernière MàJ :   11/12/2019
  *******************************************************************************************************/
 /***** *****    INCLUSIONS ET SCRIPTS   ***** *****/
 
@@ -30,23 +30,29 @@ function fctDisplayWebFonts($objPageInfos){
         $strFontFamily = "font-family:'" . $arrWebFonts[$i]['Name'] . "';";
         $strFontLinkUrl = $objPageInfos->getConfigPath() . "fonts/" . $arrWebFonts[$i]['Directory'] . "/" . $arrWebFonts[$i]['Demo'];
         $strFontLinkTitle = "Voir la page d'exemple de la font : '" . $arrWebFonts[$i]['Name'] . "'";
+        $strLinkFontKit = "../files/fontskits/" . $arrWebFonts[$i]['Directory'] . ".rar";
+        $strLinkFontKitTitle = "Téléchager le fontkit de : '" . $arrWebFonts[$i]['Name'] . "'";
 ?>
 <!-- -- -- -- Article webfont : <?php echo $arrWebFonts[$i]['Name'] . " (id: " . $i . ")";?> -- -- -- -->
       <article class="row">
        <div class="col-xl-4 col-lg-4 wft_infos">
         <div class="row">
-         <div class="col-xl-4 col-lg-4 wft_index"><span class="badge badge_custom"><?php echo $i;?></span></div>
-         <div class="col-xl-8 col-lg-8 wft_name">
-          <a href="<?php echo $strFontLinkUrl;?>" title="<?php echo $strFontLinkTitle;?>" target="_blank">
-           <?php echo $arrWebFonts[$i]['Name'];?>
-          </a>
+         <div class="col-xl-3 col-lg-3 wft_index"><span class="badge badge_custom"><?php echo $i;?></span></div>
+         <div class="col-xl-9 col-lg-9 wft_name"><?php echo $arrWebFonts[$i]['Name'];?></div>
+<!-- -- -- -- Lien vers la page de démo de : <?php echo $arrWebFonts[$i]['Name'];?> -- -- -- -->
+         <div class="offset-xl-1 col-xl-4 offset-lg-1 col-lg-4 wft_link">
+          <a href="<?php echo $strFontLinkUrl;?>" title="<?php echo $strFontLinkTitle;?>" target="_blank">Démo</a>
+         </div>
+<!-- -- -- -- Lien vers le fontkit de : <?php echo $arrWebFonts[$i]['Name'];?> -- -- -- -->
+         <div class="offset-xl-1 col-xl-5 offset-lg-1 col-lg-5 wft_link">
+          <a href="<?php echo $strLinkFontKit;?>" title="<?php echo $strLinkFontKitTitle;?>"><span class="fa fa-sign-in-alt"></span>&nbsp;Fontkit</a>
          </div>
         </div>
        </div>
+<!-- -- -- -- Exemple de la font : <?php echo $arrWebFonts[$i]['Name'];?> -- -- -- -->
        <div class="col-xl-8 col-lg-8 wft_example">
         <p style="<?php echo $strFontFamily;?>"><?php echo $strExampleText;?></p>
        </div>
-       
       </article>
 <?php        
     }
