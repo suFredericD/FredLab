4113f62be3111cbd9e24a3ff1ca0a64938682460
Fred <frederic.daniau.pro@gmail.com>
2020-11-07 15:34:41 +0100

    Refonte du menu page 'Cryptos' 	Animation du menu 	Ajout de contenu : sites avec descritions, logos, url de parrainage... 	Animation du bandeau d'accueil de la page (show/hide)
    	modifié :         config/css/crypto_main.css
    	nouveau fichier : media/logos/binance_black.jpg
    	nouveau fichier : media/logos/brave.jpg
    	nouveau fichier : media/logos/bravebat.png
    	nouveau fichier : media/logos/cmg.png
    	nouveau fichier : media/logos/coinbase.png
    	nouveau fichier : media/logos/cointiply.png
    	nouveau fichier : media/logos/exs.png
    	nouveau fichier : media/logos/faucetcrypto.png
    	nouveau fichier : media/logos/rollercoin.jpeg
    	modifié :         pages/crypto_main.php
    	modifié :         scripts/paging/cryptos.js

--- pages/crypto_main.php
+++ pages/crypto_main.php
@@ -8,7 +8,7 @@
  *              Contexte :   php 7.4
  *              Fonction :   page principale sur les cryptos
  *   Date mise en oeuvre :   02/11/2020
- *          Dernière MàJ :   03/11/2020
+ *          Dernière MàJ :   07/11/2020
  *********************************************************************************/
 /***** *****    INCLUSIONS ET SCRIPTS   ***** *****/
 require("../scripts/admin/variables.php");                  // Variables globales du site
@@ -50,44 +50,46 @@ $strWelcomeText = "<span class=\"far fa-hand-point-left\"></span>&nbsp;&nbsp;&nb
                 . "<span class=\"far fa-hand-point-left\"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"
                 . "Sélectionner une catégorie pour afficher les articles correspondants...<hr>";
 ?>
-    <h1 class="offset-xl-1 col-xl-11 offset-lg-2 col-lg-10 offset-md-3 col-md-9 offset-sm-3 col-sm-9 offset-4 col-8 no-bg-no-bd">Les crypto-monnaies</h1>
-    <section class="offset-xl-2 col-xl-10 offset-lg-3 col-lg-9 offset-md-3 col-md-9 offset-sm-3 col-sm-9 offset-4 col-8" id="secCryptos">
+    <h1 class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 no-bg-no-bd">Les crypto-monnaies</h1>
+    <section class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12" id="secCryptos">
+     <div class="row">
 <!-- -- -- -- -- -- -- MENU DES CRYPTOS APPLICATIONS -- -- -- -- -- -- -->
-     <div id="cryMenu">
-      <div class="row">
+      <div id="cryMenu" class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-4">
+       <div class="row">
 <?php
         for ($i=1;$i<=count($arrAllCryptoAppTypes);$i++){
                 $strInnerHTML = $arrAllCryptoAppTypes[$i]['Icon'] . "&nbsp;" . $arrAllCryptoAppTypes[$i]['Name'];
 ?>
-        <div id="<?php echo $arrAllCryptoAppTypes[$i]['DOM'];?>" class="col-xl-12" title="<?php echo $arrAllCryptoAppTypes[$i]['Description'];?>"><?php echo $strInnerHTML;?></div>
-        <div  class="offset-xl-2 col-xl-8 offset-lg-1 col-lg-10 offset-md-1 col-md-10">
-         <div class="row">
+         <div id="<?php echo $arrAllCryptoAppTypes[$i]['DOM'];?>" class="col-xl-12" title="<?php echo $arrAllCryptoAppTypes[$i]['Description'];?>"><?php echo $strInnerHTML;?></div>
+         <div id="crRub<?php echo $i;?>" class="col-xl-8-hidden col-lg-10-hidden col-md-10-hidden col-12-hidden">
+          <div class="row">
 <?php
                 for ($j=1;$j<=$intCryptoApps;$j++) {
                         if($arrAllCryptoApp[$j]['TypeId']==$arrAllCryptoAppTypes[$i]['Id']) {
 ?>
-           <div class="col-xl-12" title="<?php echo $arrAllCryptoApp[$j]['Resume'];?>">
+           <div class="col-xl-11" title="<?php echo $arrAllCryptoApp[$j]['Resume'];?>">
             <img src="../media/logos/<?php echo $arrAllCryptoApp[$j]['Logo'];?>" class="img-fluid crMenuLogos">
            </div>
 <?php
                         }
                 }
 ?>
+          </div>
          </div>
-        </div>
 <?php
         }
 ?>
 
-      </div>
-     </div>
-<!-- -- -- -- -- -- -- CONTENU DES CRYPTOS APPLICATIONS -- -- -- -- -- -- -->
-      <article id="cryContent" class="col-xl-12">
-       <div class="row">
-        <div id="crySubTitle" class="col-xl-12"><p><?php echo $strWelcomeText;?></p></div>
-        <div id="cryText" class="col-xl-12"><p></p></div>
+        </div>
        </div>
-      </article>
+<!-- -- -- -- -- -- -- CONTENU DES CRYPTOS APPLICATIONS -- -- -- -- -- -- -->
+       <article id="cryContent" class="col-xl-9 col-lg-9 col-md-8 col-sm-8 col-8">
+        <div class="row">
+         <div id="crySubTitle" class="col-xl-12"><p><?php echo $strWelcomeText;?></p></div>
+         <div id="cryText" class="col-xl-12"><p></p></div>
+        </div>
+       </article>
+      </div>
      </div>
     </section>
 <?php
--- scripts/paging/cryptos.js
+++ scripts/paging/cryptos.js
@@ -8,7 +8,7 @@
  *              Contexte :   JavaScript
  *              Fonction :   script de mise en page et animation de la page crypto_main.php
  *   Date mise en oeuvre :   02/11/2020
- *          Dernière MàJ :   03/11/2020
+ *          Dernière MàJ :   07/11/2020
  *********************************************************************************************/
 /* *** *** *** ELEMENTS *** *** *** */
 const lnkWallets = document.getElementById("lnkWallets");         // Bouton multiwallets
@@ -18,15 +18,22 @@ const lnkPTC = document.getElementById("lnkPTC");                 // Bouton PTC
 const lnkFaucets = document.getElementById("lnkFaucets");         // Bouton faucets
 const lnkGames = document.getElementById("lnkGames");             // Bouton jeux
 
-const secWallets = document.getElementById("secWallets");         // 
-
+const eltSubTitle = document.getElementById("crySubTitle");       // Sous-tire de section 'contenu'
+const eltContent = document.getElementById("cryText");            // Section 'contenu'
 /* *** *** *** VARIABLES *** *** *** */
 var bolDisplayWallets = false;               // Controller d'affichage : section multiwallets
+var bolDisplayExchanges = false;             // Controller d'affichage : section exchanges
+var bolDisplayBrowser = false;               // Controller d'affichage : section navigateurs
+var bolDisplayPTC = false;                   // Controller d'affichage : section PTC
+var bolDisplayFaucets = false;               // Controller d'affichage : section faucets
+var bolDisplayGames = false;                 // Controller d'affichage : section jeux
 
 /* *** *** *** PARAMETRES *** *** *** */
 const intDisplayFadeIn = 2000;      // Timer animation show
 const intDisplayFadeOut = 1150;     // Timer animation hide
+const intAmimationMenu = 750;       // Timer animation menu show/hide
 
+const strWelcomeText = eltSubTitle.innerHTML;
 /* *** *** *** ANIMATIONS *** *** *** */
 // Animation du titre header
 const intAmimationStartDelay = 500;
@@ -38,20 +45,112 @@ $( document ).ready(function() {
 
 });
 /* *** *** *** FONCTIONS *** *** *** */
+//  Fonction d'affichage/masquage du header de la section 'contenu'
+//  Paramètres          : bolShowHide
+//  Valeur de retour    : none
+function fctShowHideSubTitle(bolShowHide){
+    if(bolShowHide == true){
+        $("#crySubTitle").hide("pulsate", intAmimationStartDelay);
+    } else {
+        $("#crySubTitle").show("pulsate", intAmimationStartDelay);
+    }
+}
 //  Fonction d'affichage/masquage de la section 'multiwallets'
 //  EvenListener        : lnkWallets
 //  Paramètres          : none
 //  Valeur de retour    : none
-function fctDisplayWallets(){
-    if ( bolDisplayWallets != true ) {
-        $("#secWallets").show("fold", intDisplayFadeIn);
+function fctShowHideWallets(){
+    if(bolDisplayWallets == false){
+        $("#crRub1").show("fold", intAmimationMenu);
         bolDisplayWallets = true;
+        fctShowHideSubTitle(true);
     } else {
-        $("#secWallets").hide("fold", intDisplayFadeOut);
+        $("#crRub1").hide("fold", intAmimationMenu);
         bolDisplayWallets = false;
+        fctShowHideSubTitle(false);
+    }
+}
+//  Fonction d'affichage/masquage de la section 'exchanges'
+//  EvenListener        : lnkExchanges
+//  Paramètres          : none
+//  Valeur de retour    : none
+function fctShowHideExchanges(){
+    if(bolDisplayExchanges == false){
+        $("#crRub2").show("fold", intAmimationMenu);
+        bolDisplayExchanges = true;
+        fctShowHideSubTitle(true);
+    } else {
+        $("#crRub2").hide("fold", intAmimationMenu);
+        bolDisplayExchanges = false;
+        fctShowHideSubTitle(false);
+    }
+}
+//  Fonction d'affichage/masquage de la section 'browsers'
+//  EvenListener        : lnkBrowser
+//  Paramètres          : none
+//  Valeur de retour    : none
+function fctShowHideBrowser(){
+    if(bolDisplayBrowser == false){
+        $("#crRub3").show("fold", intAmimationMenu);
+        bolDisplayBrowser = true;
+        fctShowHideSubTitle(true);
+    } else {
+        $("#crRub3").hide("fold", intAmimationMenu);
+        bolDisplayBrowser = false;
+        fctShowHideSubTitle(false);
+    }
+}
+//  Fonction d'affichage/masquage de la section 'PTC'
+//  EvenListener        : lnkPTC
+//  Paramètres          : none
+//  Valeur de retour    : none
+function fctShowHidePTC(){
+    if(bolDisplayPTC == false){
+        $("#crRub4").show("fold", intAmimationMenu);
+        bolDisplayPTC = true;
+        fctShowHideSubTitle(true);
+    } else {
+        $("#crRub4").hide("fold", intAmimationMenu);
+        bolDisplayPTC = false;
+        fctShowHideSubTitle(false);
+    }
+}
+//  Fonction d'affichage/masquage de la section 'faucets'
+//  EvenListener        : lnkFaucets
+//  Paramètres          : none
+//  Valeur de retour    : none
+function fctShowHideFaucets(){
+    if(bolDisplayFaucets == false){
+        $("#crRub5").show("fold", intAmimationMenu);
+        bolDisplayFaucets = true;
+        fctShowHideSubTitle(true);
+    } else {
+        $("#crRub5").hide("fold", intAmimationMenu);
+        bolDisplayFaucets = false;
+        fctShowHideSubTitle(false);
+    }
+}
+//  Fonction d'affichage/masquage de la section 'jeux'
+//  EvenListener        : lnkGames
+//  Paramètres          : none
+//  Valeur de retour    : none
+function fctShowHideGames(){
+    if(bolDisplayGames == false){
+        $("#crRub6").show("fold", intAmimationMenu);
+        bolDisplayGames = true;
+        fctShowHideSubTitle(true);
+    } else {
+        $("#crRub6").hide("fold", intAmimationMenu);
+        bolDisplayGames = false;
+        fctShowHideSubTitle(false);
     }
 }
 /* *** *** *** APPELS DE FONCTIONS *** *** *** */
 
 /* *** *** *** EVENT LISTENERS *** *** *** */
-lnkWallets.addEventListener("click", fctDisplayWallets);       // Clic : bouton multiwallets
+lnkWallets.addEventListener("click", fctShowHideWallets);           // click : multiWallets
+lnkExchanges.addEventListener("click", fctShowHideExchanges);       // click : exchanges
+lnkBrowser.addEventListener("click", fctShowHideBrowser);           // click : browsers
+lnkPTC.addEventListener("click", fctShowHidePTC);                   // click : PTC
+lnkFaucets.addEventListener("click", fctShowHideFaucets);           // click : faucets
+lnkGames.addEventListener("click", fctShowHideGames);               // click : jeux
